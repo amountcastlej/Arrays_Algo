ec63be90902e73aff559a54f83c1a1e9277a7f26
Adam Joines <amountcastlej@gmail.com>
2021-10-22 22:17:53 -0400

    "intermediate algos added"

--- ArrAlgo.js
+++ ArrAlgo.js
@@ -18,3 +18,36 @@ for(var i=0; i<testArr.length; i++){
 console.log(testArr);
 
 
+//3. Always Hungry
+//Write a function that is given an array and each time the value is "food" it should console.log "yummy". If "food" was not present in the array console.log "I'm hungry" once
+function alwaysHungry(arr){
+    var foodCount = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] == "food"){
+            console.log("yummy")
+            foodCount++;
+        }
+    }
+    if (foodCount == 0){
+        console.log("I'm hungry")
+    }
+}
+//alwaysHungry([3.14, "food", "pie", true, "food"]);
+//alwaysHungry([4, 1, 5, 7, 2]);
+
+//4. High pass Filter
+// Given an array and a value cutoff, return a new array containing only the values larger than cutoff
+
+function highPass(arr, cutoff){
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] > cutoff){
+            filteredArr.push(arr[i]);
+        } else {
+            continue;
+        }
+    }
+    return filteredArr; 
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result);
