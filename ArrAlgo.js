7e0dfaa18b05fe0445585f3920b0edc7796ce61c
Adam Joines <amountcastlej@gmail.com>
2021-10-26 22:23:04 -0400

    "added algo"

--- ArrAlgo.js
+++ ArrAlgo.js
@@ -71,3 +71,20 @@ function betterThanAverage(arr){
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
 console.log(result)
+
+function flatten(arr){
+    var newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0]){
+            for(let j=0; j < arr[i].length; j++){
+                newArr.push(arr[i][j])
+            }
+        }
+        else {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+var myArr = [1, [2,3], 4,[]]
+console.log(flatten(myArr))
